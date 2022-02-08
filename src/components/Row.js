3faeb62a362ca3e882a2654df7f7ca7bb91ef53a
GhazalehNiazi <ghazaleniazisabet02@gmail.com>
2022-02-08 20:47:24 +0330

    made background-img and poster-size dynamic

--- src/components/Row.js
+++ src/components/Row.js
@@ -4,17 +4,26 @@ import classes from "./Row.module.css";
 
 const Row = ({ fetchUrl, Active }) => {
   const baseURL = "https://image.tmdb.org/t/p/original/";
-  const [currentMovie, setCurrentMovie] = useState(0);
   const [movies, setMovies] = useState([]);
-
+  const [currentMovie, setCurrentMovie] = useState([]);
+  // let currentMovie;
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
       setMovies(request.data.results);
+      setCurrentMovie(request.data.results[0]);
       return request;
     }
     fetchData();
   }, [fetchUrl]);
+
+  const clickHandler = (selectedMovie) => {
+    console.log(selectedMovie);
+    const index = movies.indexOf(selectedMovie);
+    console.log(index);
+    const movie = movies[index];
+    setCurrentMovie(movie);
+  };
 
   return (
     <React.Fragment>
@@ -22,21 +31,26 @@ const Row = ({ fetchUrl, Active }) => {
         <div
           className={classes.main}
           style={{
+            transition:'all 450ms ease-in',
             objectFit: "contain",
             backgroundSize: "cover",
             backgroundPosition: "center center",
-            backgroundImage: `url('https://image.tmdb.org/t/p/original/${movies[0]?.backdrop_path}')`,
+            backgroundImage: `url('https://image.tmdb.org/t/p/original/${currentMovie?.backdrop_path}')`,
           }}
         >
           <div className={classes.posters}>
             {movies.map((movie) => {
+              console.log(movie === currentMovie);
               return (
-                  <img
-                    className={classes.postersImage}
-                    src={`${baseURL}${movie.poster_path}`}
-                    key={movie.id}
-                    alt={movie.name}
-                  />
+                <img
+                  onClick={clickHandler.bind(this, movie)}
+                  className={`${classes.posterImg} ${
+                    movie === currentMovie && `${classes.active}`
+                  }`}
+                  src={`${baseURL}${movie.poster_path}`}
+                  key={movie.id}
+                  alt={movie.name}
+                />
               );
             })}
           </div>
