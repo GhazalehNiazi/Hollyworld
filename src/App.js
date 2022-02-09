ac6237dc3a4090c9a2b9eca000e9812be7dcba55
GhazalehNiazi <ghazaleniazisabet02@gmail.com>
2022-02-09 17:20:42 +0330

    added logo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import requests from "./API/request";
 import MainPage from "./components/MainPage";
 import Genres from "./components/Genres";
 import { useState } from "react";
-
+import logo from "./assets/logo.png";
 function App() {
   const [title, setTitle] = useState("Top Rated");
   const clickHandler = (selectedTitle) => {
@@ -13,18 +13,22 @@ function App() {
 
   return (
     // prettier-ignore
-    <div className="App">
+    <div className="app">
+        <div className='nav'> 
+            <div className='logo'><img src={logo}/> 
+            </div>
+         </div>
       <div className="container">
         <Genres onSelect={clickHandler.bind(this)} title="Trending" Active={title === 'Trending'|| false}/>
         <Genres onSelect={clickHandler.bind(this)} title="Top Rated" Active={title === 'Top Rated' || false}/>
-        <Genres onSelect={clickHandler.bind(this)} title="netflix Originals" Active={title === 'netflix Originals' || false}/>
+        <Genres onSelect={clickHandler.bind(this)} title="Originals" Active={title === 'Originals' || false}/>
       </div>
       <MainPage title="Trending" fetchUrl={requests.fetchTrending} Active={title === 'Trending' || false} />
       <MainPage title="Top Rated" fetchUrl={requests.fetchTopRated} Active={title === 'Top Rated' || false}/>
       <MainPage
-        title="netflix Originals"
+        title="Originals"
         fetchUrl={requests.fetchNetflixOriginals}
-        Active={title === 'netflix Originals' || false}
+        Active={title === 'Originals' || false}
       />
     </div>
   );
